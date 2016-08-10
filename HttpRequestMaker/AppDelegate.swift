0e9b787a5db48da8de2a3008b8bd9646b5b0a007
Hidden Butler Development <hiddenbutler@hiddens-imac.local>
2016-08-10 14:55:02 -0600

    Initial Commit

---
+++ HttpRequestMaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HttpRequestMaker
+//
+//  Created by Hidden Butler Development on 8/10/16.
+//  Copyright © 2016 Taylor Robbins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
